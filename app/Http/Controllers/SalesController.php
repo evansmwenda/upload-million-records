156be0d0190307f448096c012997a00f6724882f
evansmwenda <evansmwenda.em@gmail.com>
2023-10-18 16:43:27 +0300

    still bugged

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sales;
 use Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
-    //
+    public function index(){
+        return view('upload-file');
+    }
+
+    public function store(Request $request){
+        if(request()->has('mycsv')){
+            $file = $request->file('mycsv');
+            // dd(request()->mycsv);
+    
+            // $data = array_map('str_getcsv',file(request()->mycsv)); 
+            $data = file(request()->mycsv);
+            $header = $data[0];
+            unset($data[0]);
+            
+
+            //chunking files
+            $chunks = array_chunk($data,1000);
+            //convert chunk into file
+            foreach($chunks as $key => $chunk){
+                $name = "/tmp{$key}.csv";
+                $path = resource_path('temp');
+                file_put_contents($path . $name, $chunk);
+            }
+
+            // dd($chunks[0]);
+            // dd(count($chunks));
+
+
+
+            // foreach($data as $sale){
+            //     $saleData = (array_combine($header,$sale));
+            //     Sales::create($saleData);
+            // }
+            return 'Done';
+    
+    
+            // echo("path->".$file->getRealPath());
+            // dd("\nname->".$file->getClientOriginalName());
+            // $data = file($file);
+            // dd($data);
+            // dd(request()->mycsv);
+
+        
+        }
+        return "no file has been submitted";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SalesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Validator;
@@ -19,77 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/upload',function(){
-    return view('upload-file');
-});
+Route::get('/upload',[SalesController::class,'index']);
 
-Route::post('/upload',function(Request $request){
-    if(request()->has('mycsv')){
-        $file = $request->file('mycsv');
-
-        $data = array_map('str_getcsv',file(request()->mycsv)); 
-        $header = $data[0];
-        dd($header);
-        unset($data[0]);
-        return $data;
-
-
-        // echo("path->".$file->getRealPath());
-        // dd("\nname->".$file->getClientOriginalName());
-        // $data = file($file);
-        // dd($data);
-        // dd(request()->mycsv);
-
-        // $rules = array(
-        //     'attachment' => 'mimes:jpeg,png,jpg,gif,pdf|max:1000',
-        // );
-        // $messages = array(
-        //     'attachment' => ' Image need Less then 1Mb.',
-        // );
-
-        // $validator = Validator::make($request->all(), $rules, $messages);
-
-        // if ($validator->fails()){
-        //     dd("validator has failed");
-        // }else{
-        //     dd("validator was successfull");
-        // }
-
-
-        // echo '<br>';
-        // echo '<br>';echo '<br>';echo '<br>';
-   
-      //Display File Name
-    //   echo 'File Name: '.$file->getClientOriginalName();
-    //   echo '<br>';
-   
-    //   //Display File Extension
-    //   echo 'File Extension: '.$file->getClientOriginalExtension();
-    //   echo '<br>';
-   
-    //   //Display File Real Path
-    //   echo 'File Real Path: '.$file->getRealPath();
-    //   echo '<br>';
-
-    //   $path = $request->file('mycsv')->store('avatars');
- 
-    //     return $path;
-   
-    //   //Display File Size
-    //   echo 'File Size: '.$file->getSize();
-    //   echo '<br>';
-   
-    //   //Display File Mime Type
-    //   echo 'File Mime Type: '.$file->getMimeType();
-    //   echo '<br>';
-   
-
-    //   Move Uploaded File
-    //   $destinationPath = 'uploads';
-    //   $file->move($destinationPath,$file->getClientOriginalName());
-    
-    }
-
-
-    return "no file has been submitted";
-});
+Route::post('/upload',[SalesController::class,'store']);
